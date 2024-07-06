51b9a61a62e0248aa01c23d3d80dd77793c90a3f
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-06 11:56:20 -0400

    implemented the update route

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -10,7 +10,7 @@ transactionController.get("/", (req, res) => {
 //show
 transactionController.get("/:id", (req, res) => {
 	const { id } = req.params;
-	const transaction = transactionModel.find((tran) => tran.id === +id);
+	const transaction = transactionModel.find((tran) => tran.id === Number(id));
 	console.log(nanoid(4), "el id");
 	if (transaction) {
 		res.status(200).json(transaction);
@@ -26,6 +26,19 @@ transactionController.post("/", (req, res) => {
 	transactionModel.push({ id: transactionModel.length + 1, ...req.body });
 	const addedTranx = transactionModel[transactionModel.length - 1];
 	res.status(200).json(addedTranx);
+});
+
+//update
+transactionController.put("/:id", (req, res) => {
+  const {id} =  req.params
+	const idx = transactionModel.findIndex((tranx) => tranx.id === Number(id));
+
+	if (idx !== -1) {
+		transactionModel[idx] = { ...transactionModel[idx], ...req.body };
+		res.status(200).json(transactionModel[idx]);
+	} else {
+		res.status(404).json({ error: `Transaction with id:${id} not found` });
+	}
 });
 
 module.exports = transactionController;
