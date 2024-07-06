8a71bd967992117a370dce74ca6cc7f241b6e467
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-06 11:47:02 -0400

    created index a, show and create routes

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,10 +1,31 @@
+// const { nanoid } = require("nanoid");
 const express = require("express");
-
 const transactionController = express.Router();
 const transactionModel = require("../models/transactionModel");
 
 transactionController.get("/", (req, res) => {
 	res.status(200).json(transactionModel);
+});
+
+//show
+transactionController.get("/:id", (req, res) => {
+	const { id } = req.params;
+	const transaction = transactionModel.find((tran) => tran.id === +id);
+	console.log(nanoid(4), "el id");
+	if (transaction) {
+		res.status(200).json(transaction);
+	} else {
+		res.status(404).json({ error: `Transaction with id:${id} not found` });
+	}
+});
+
+//create
+
+transactionController.post("/", (req, res) => {
+	// console.log(nanoid(4))
+	transactionModel.push({ id: transactionModel.length + 1, ...req.body });
+	const addedTranx = transactionModel[transactionModel.length - 1];
+	res.status(200).json(addedTranx);
 });
 
 module.exports = transactionController;
